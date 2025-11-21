d43e878f908a1d61c41a2592f85bbfa69b38af4a
ST10375943 <166000514+ST10375943@users.noreply.github.com>
2025-11-21 23:24:37 +0200

    Database Migrations

---
+++ CMCS.Web/Migrations/20251121183940_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMCS.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Claims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LecturerName = table.Column<string>(type: "TEXT", nullable: false),
+                    LecturerEmail = table.Column<string>(type: "TEXT", nullable: false),
+                    Month = table.Column<int>(type: "INTEGER", nullable: false),
+                    Year = table.Column<int>(type: "INTEGER", nullable: false),
+                    HoursWorked = table.Column<decimal>(type: "TEXT", nullable: false),
+                    HourlyRate = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CoordinatorComments = table.Column<string>(type: "TEXT", nullable: true),
+                    HRComments = table.Column<string>(type: "TEXT", nullable: true),
+                    SubmittedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Claims", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Claims");
+        }
+    }
+}
