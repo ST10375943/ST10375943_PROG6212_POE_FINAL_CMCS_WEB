80bedc37835fd31ef69c6ecf7004887c287a0804
ST10375943 <166000514+ST10375943@users.noreply.github.com>
2025-11-21 23:23:59 +0200

    Added Database Context file

---
+++ CMCS.Web/Data/CMCSContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CMCS.Web.Models;
+
+namespace CMCS.Web.Data
+{
+    public class CMCSContext : IdentityDbContext
+    {
+        public CMCSContext(DbContextOptions<CMCSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Claim> Claims { get; set; }
+    }
+}
